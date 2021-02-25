7916c2c3fbace23a4223eddc98ceeafabfa48c83
jbaxe2 <jbaxe2@gmail.com>
2021-02-25 13:56:20 -0500

    refactored some solutions

--- src/main/java/SurrealCheck.java
+++ src/main/java/SurrealCheck.java
@@ -1,10 +1,16 @@
+import java.util.*;
+
 public class SurrealCheck {
   public boolean isSurreal (String left, String right) {
-    if ("".equals (left) || "".equals (right)) {
+    if ("".equals(left) || "".equals(right)) {
       return true;
     }
 
-    double[] leftNums, rightNums;
+    return _isSurreal (left, right);
+  }
+
+  private boolean _isSurreal (String left, String right) {
+    List<Double> leftNums, rightNums;
 
     try {
       leftNums = _convertStringsToDoubles (_splitNumbersStr (left));
@@ -16,17 +22,14 @@ public class SurrealCheck {
     return _checkLeftVsRight (leftNums, rightNums);
   }
 
-  private double[] _convertStringsToDoubles (String[] numbersStrs) {
-    var numDoubles = new double[numbersStrs.length];
+  private List<Double> _convertStringsToDoubles (String[] numbersStrs) {
+    List<Double> numDoubles = new ArrayList<>();
 
-    for (int i=0; i<numbersStrs.length; i++) {
-      String number = numbersStrs[i];
-
-      if (number.contains ("/")) {
-        numDoubles[i] = _convertFraction (number);
-      } else {
-        numDoubles[i] = Double.parseDouble (number);
-      }
+    for (String numberStr : numbersStrs) {
+      numDoubles.add (
+        numberStr.contains ("/") ?
+          _convertFraction (numberStr) : Double.parseDouble (numberStr)
+      );
     }
 
     return numDoubles;
@@ -36,16 +39,10 @@ public class SurrealCheck {
     return numbersStr.split (" ");
   }
 
-  private boolean _checkLeftVsRight (double[] left, double[] right) {
-    for (double leftNum : left) {
-      for (double rightNum : right) {
-        if (leftNum >= rightNum) {
-          return false;
-        }
-      }
-    }
-
-    return true;
+  private boolean _checkLeftVsRight (List<Double> left, List<Double> right) {
+    return left.stream().noneMatch (
+      leftNum -> right.stream().anyMatch (rightNum -> leftNum >= rightNum)
+    );
   }
 
   private double _convertFraction (String fraction) {
