9eadf5e574fce2a7decd2f4ae3e1c0c9c2eab35f
jbaxe2 <jbaxe2@gmail.com>
2021-03-01 19:05:02 -0500

    removed adder

--- src/main/java/Adder.java
+++
@@ -1,9 +0,0 @@
-public class Adder {
-  public int add (int first, int second) {
-    return first + second;
-  }
-
-  public double add (double first, double second) {
-    return first + second;
-  }
-}
